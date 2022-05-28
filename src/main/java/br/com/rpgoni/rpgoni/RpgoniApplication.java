fffa9f7fc7fcf71e50c6d6b330b79c0d847a6626
BNYKARUS <ADLERFCLIMA17@GMAIL.COM>
2022-05-28 19:39:43 -0300

    adicionando projeto

---
+++ src/main/java/br/com/rpgoni/rpgoni/RpgoniApplication.java
@@ -0,0 +1,13 @@
+package br.com.rpgoni.rpgoni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgoniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgoniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rpgoni/rpgoni/RpgoniApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rpgoni.rpgoni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgoniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
